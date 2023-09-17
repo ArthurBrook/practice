092af2e5ed02c6422686c7ee7cc90388b5b9dbed
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2023-09-17 11:33:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,26 @@
  *? інакше відобразити:"Не знаєте? ECMAScript!"
  */
 
-const jsName = "ECMAScript";
-const userUnswer = prompt("Яка офіційна назва JavaScript?").trim();
-    if (userUnswer === jsName) {
-        alert ("Вірно!");
+// const jsName = "ECMAScript";
+// const userUnswer = prompt("Яка офіційна назва JavaScript?").trim();
+// if (userUnswer === jsName) {
+//     alert("Вірно!");
+// }
+// else {
+//     alert("Не знаєте? ECMAScript!");
+// }
+
+
+/**
+*? Напишіть цикл, який виводить у консоль
+*? всі парні числа  від max до min  за спаданням
+*/
+
+const max = 50;
+const min = 23;
+
+for (let i = max; i >= min; i -= 1) {
+    if(i % 2 !== 0) {
+        console.log(i);
     }
-    else {
-        alert ("Не знаєте? ECMAScript!");
-    }
+}
